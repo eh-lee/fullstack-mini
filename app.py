78a4e603d13e6bfc6ef4e89f7d7c42090659da41
2ggg <larunaru0386@gmail.com>
2023-02-12 04:01:10 +0900

    url 크롤링

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, jsonify
 
-
 import requests
 from bs4 import BeautifulSoup
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.n2wdmo7.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
-app = Flask(__name__)
+app = Flask(__name__,static_folder="./static/")
 
 
 @app.route('/')
@@ -25,7 +26,6 @@ def webtoon_post():
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url_receive, headers=headers)
 
-
     doc = {
         'star':star_receive,
         'comment':comment_receive
@@ -34,6 +34,40 @@ def webtoon_post():
 
     return jsonify({'msg':'업로드 성공'})
 
+@app.route('/webtoon-list', methods=["GET"])
+def webtoon_list():
+    url_receive = list(db.webtoon.find({}, {'_id':False}))
+    webtoon_list = []
+
+    for item in url_receive:
+        url = item['url']
+        naver_detail_data = requests.get(url, headers=headers)
+        detail_soup = BeautifulSoup(naver_detail_data.text, 'html.parser')
+        webtoon = detail_soup.select('#content > div.comicinfo')
+
+        for item in webtoon:
+            webtoon_title = item.select_one('div.detail > h2 > span.title').text
+            webtoon_img = item.select_one('div.thumb > a > img')['src']
+            webtoon_desc = item.select_one('div.detail > p:nth-child(2)').text
+            webtoon_url = 'https://comic.naver.com' + item.select_one('div.thumb > a')['href']
+            webtoon_writer = item.select_one('div.detail > h2 > span.wrt_nm').text.strip()
+            webtoon_genre = item.select_one('div.detail > p.detail_info > span.genre').text
+
+            webtoon_list.append(
+                {
+                    'title': webtoon_title,
+                    'img': webtoon_img,
+                    'desc': webtoon_desc,
+                    'url': webtoon_url,
+                    'writer': webtoon_writer,
+                    'genre': webtoon_genre
+                }
+            )
+
+            print(webtoon_url)
+
+    print(webtoon_list)
+    return jsonify({'webtoon-list':webtoon_list})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
---
+++ static/js/webtoon_list.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function () {
+        show_webtoon();
+    });
+
+    function show_webtoon(){
+        $.ajax({
+            type: "GET",
+            url: "/webtoon-list",
+            data: {},
+            success: function (response) {
+                let webtoon_list = response['webtoon-list'];
+
+                console.log(webtoon_list);
+
+                for (item in webtoon_list) {
+                    console.log(item);
+                    var img = webtoon_list[item]['img'];
+                    var url = webtoon_list[item]['url'];
+                    var title = webtoon_list[item]['title'];
+                    var genre = webtoon_list[item]['genre'];
+                    var desc = webtoon_list[item]['desc'];
+
+                    console.log(img, url, title, genre, desc);
+
+                    var temp_html =
+                        `<li>
+                            <a href="${url}">
+                                <div class="webtoon-box">
+                                    <img src="${img}" alt="웹툰 이미지">
+                                    <h6>${title}</h6>
+                                    <span>${genre}</span>
+                                    <p>${desc}</p>
+                                </div>
+                            </a>
+                        </li>`;
+
+                    $('#webtoons-list').append(temp_html);
+                }
+            }
+        });
+    }
